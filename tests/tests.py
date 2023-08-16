358e622379c41a5325508082984b371ac882e1e6
Eftal Gezer <eftalgezer@gmail.com>
2023-08-16 21:18:23 +0300

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -40,6 +40,27 @@ def test_DummyPackage():
 
 
 def test_DummyPackage__errors():
+    """
+    Test scenarios for handling errors in the DummyPackageTester class.
+
+    This function tests different error scenarios that may arise when using the DummyPackageTester
+    class to simulate errors during package installation and uninstallation.
+
+    Test cases:
+    1. Test ImportError when a required dependency source directory is missing.
+    2. Test ImportError when the main package source directory is missing.
+    3. Test ImportError and FileNotFoundError during uninstallation of a dependency.
+    4. Test ImportError during uninstallation of the main package.
+
+    Each test case uses the TestCase().assertRaises context manager to ensure that the expected
+    exceptions are raised during the specified scenarios.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     tester = DummyPackageTester("package1", requirements=["package2"])
     with tester:
         assert tester.package
