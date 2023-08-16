be4b9a3ab1d6fa1e3944a07e7a46c2802284b8ce
Eftal Gezer <eftalgezer@gmail.com>
2023-08-16 14:11:23 +0300

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,6 @@
 This module provides test scenarios using the DummyPackageTester class to test the DummyPackage class.
 """
 
-import shutil
 from .testers import DummyPackageTester, __exit___tester
 
 
@@ -15,11 +14,11 @@ def test_DummyPackage():
         assert tester.package
         assert tester.package["name"] == "package1"
         assert tester.package["deps"][0]["name"] == "package2"
-        assert tester.install_tester()
+        tester.install_tester()
         assert tester.package["is_installed"]
-        assert tester.uninstall_tester()
+        tester.uninstall_tester()
         assert not tester.package["is_installed"]
-        assert tester.install_tester()
+        tester.install_tester()
         assert tester.package["is_installed"]
     __exit___tester(tester)
     tester = DummyPackageTester("package1")
@@ -27,10 +26,10 @@ def test_DummyPackage():
         assert tester.package
         assert tester.package["name"] == "package1"
         assert tester.package["deps"] == []
-        assert tester.install_tester()
+        tester.install_tester()
         assert tester.package["is_installed"]
-        assert tester.uninstall_tester()
+        tester.uninstall_tester()
         assert not tester.package["is_installed"]
-        assert tester.install_tester()
+        tester.install_tester()
         assert tester.package["is_installed"]
     __exit___tester(tester)
